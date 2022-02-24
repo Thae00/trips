88a812659e52738c7f956ce1ca7338435a338219
Thae00 <mathaeoo97@gmail.com>
2022-02-24 20:14:26 +0630

    onTap event for BottomNavigationBar

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -20,11 +20,26 @@ class _MainPageState extends State<MainPage> {
     MyPage(),
   ];
 
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[0],
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
         selectedItemColor: Colors.black54,
         unselectedItemColor: Colors.grey.withOpacity(0.5),
         showUnselectedLabels: false,
